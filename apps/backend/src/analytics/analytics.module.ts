4cb16d1fd0f25e0117a0b9e7f3812e40663e3721
sveeeOW <115881236+sveeeOW@users.noreply.github.com>
2026-06-24 23:57:34 +0300

    Register forecast service

--- apps/backend/src/analytics/analytics.module.ts
+++ apps/backend/src/analytics/analytics.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AnalyticsService } from './analytics.service';
+import { ForecastService } from './forecast.service';
 import { AnalyticsController } from './analytics.controller';
 import { InvestmentsModule } from '../investments/investments.module';
 
 @Module({
   imports: [InvestmentsModule],
   controllers: [AnalyticsController],
-  providers: [AnalyticsService],
-  exports: [AnalyticsService],
+  providers: [AnalyticsService, ForecastService],
+  exports: [AnalyticsService, ForecastService],
 })
 export class AnalyticsModule {}
